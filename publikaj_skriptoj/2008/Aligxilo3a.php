08b17a9778b3fe3f505bd220907f6b0f81f84f9d
epaul <epaul@d42f5885-3628-0410-ad0c-d9650607aecb>
2008-08-19 11:15:45 +0000

    korekto por cimo #14385
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/aligilo/programo@173 d42f5885-3628-0410-ad0c-d9650607aecb

--- publikaj_skriptoj/2008/Aligxilo3a.php
+++ publikaj_skriptoj/2008/Aligxilo3a.php
@@ -19,7 +19,10 @@ if (!$_POST['pasporta_adreso'] or !$_POST['senda_adreso'])
     {
         if (!$_POST['landonomo'])
             {
-                require_once('datumbazkonekto.php');
+                echo "<!-- prafix: $prafix -->";
+
+                require_once($prafix . '/iloj/iloj.php');
+                //                require_once('datumbazkonekto.php');
                 $landonomo = eltrovu_landon($_POST['lando']);
                 echo
                     "<input type='hidden' name='landonomo' value='$landonomo' />\n";
