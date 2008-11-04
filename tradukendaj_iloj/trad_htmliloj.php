e3efd21b26244d0f3c24eb1835d0d38302be2533
epaul <epaul@d42f5885-3628-0410-ad0c-d9650607aecb>
2008-11-04 15:51:55 +0000

    nomsxangxo + dokumentado
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/aligilo/programo@269 d42f5885-3628-0410-ad0c-d9650607aecb

--- tradukendaj_iloj/trad_htmliloj.php
+++ tradukendaj_iloj/trad_htmliloj.php
@@ -5,7 +5,7 @@
    * tradukeblajn tekstojn.
    *
    * @package aligilo
-   * @subpackage iloj
+   * @subpackage tradukendaj_iloj
    * @author Martin Sawitzki, Paul Ebermann
    * @version $Id$
    * @copyright 2008 Paul Ebermann.
@@ -13,7 +13,17 @@
    */
 
 
-function donu_mankoCSS($nomo, $subnomo) {
+  /**
+   */
+
+  /**
+   * redonas HTML-atribut-kodon uzendan, se $nomo[$subnomo] aperas
+   * en $GLOBALS['mankas'].
+   *
+   * @param string $nomo
+   * @param string $subnomo
+   */
+function donu_mankoAttr($nomo, $subnomo) {
     if (is_array($GLOBALS['mankas']) and
         in_array($nomo.'['. $subnomo . ']',
                  $GLOBALS['mankas']))
@@ -24,8 +34,17 @@ function donu_mankoCSS($nomo, $subnomo) {
         return "";
 }
 
-
-function simpla_datoelektilo($nomo, $jaro_min=1930, $jaro_max=2008)
+/**
+ * kreas elektilon por dato, konsistanta el tri elektiloj por tago,
+ * monato, jaro.
+ *
+ * @param string $nomo la ĉef-nomo de la elektilo. Ni uzas
+ *                  $nomo['tago'], $nomo['monato'], $nomo['jaro'] kiel
+ *                  internaj nomoj por la sub-elektiloj.
+ * @param int $jaro_min la minimuma jaro por la jaro-elektilo.
+ * @param int $jaro_max la maksimuma jaro por la jaro-elektilo.
+ */
+function simpla_datoelektilo($nomo, $jaro_max=2008, $jaro_min=1930)
 {
 	$tagolisto = array_merge(array("-#-#-"), range(1,31));
 
@@ -39,7 +58,7 @@ function simpla_datoelektilo($nomo, $jaro_min=1930, $jaro_max=2008)
 
         
 	elektilo_simpla($nomo.'[tago]', $tagotraduklisto, $defauxlta_tago,
-                    "", 1, false, donu_mankoCSS($nomo, 'tago'));
+                    "", 1, false, donu_mankoAttr($nomo, 'tago'));
     
     $monatotraduklisto = array("-#-#-" => '(' . CH('~#monato') . ')',
                                1  => CH('~#januaro'),
@@ -60,7 +79,7 @@ function simpla_datoelektilo($nomo, $jaro_min=1930, $jaro_max=2008)
         $defaulxta_monato = "-#-#-";
  
     elektilo_simpla($nomo.'[monato]',  $monatotraduklisto,  $defauxlta_monato,
-                    "", 1, false, donu_mankoCSS($nomo, '~#monato'));
+                    "", 1, false, donu_mankoAttr($nomo, '~#monato'));
     
 
 	$jarotraduklisto = array('-#-#-' => '(' . CH('~#jaro') . ')');
@@ -71,7 +90,7 @@ function simpla_datoelektilo($nomo, $jaro_min=1930, $jaro_max=2008)
         $defaulxta_jaro = "-#-#-";
 
 	elektilo_simpla($nomo.'[jaro]', $jarotraduklisto, $defauxlta_jaro,
-                    "", 1, false, donu_mankoCSS($nomo, 'jaro'));
+                    "", 1, false, donu_mankoAttr($nomo, 'jaro'));
 
 }
 
