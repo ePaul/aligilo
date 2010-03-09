40bf69d9646a774b091d1d0837565f15f71679e2
epaul <epaul@d42f5885-3628-0410-ad0c-d9650607aecb>
2010-03-09 19:58:48 +0000

    Diagramo de la programstrukturo (tre simpla)
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/aligilo/programo@374 d42f5885-3628-0410-ad0c-d9650607aecb

--- doku/kreu-grafikojn.sh
+++ doku/kreu-grafikojn.sh
@@ -3,6 +3,8 @@
 #
 #
 
-dot -Tpng -o strukturo-malaligxkotizosistemo.png strukturo-malaligxkotizosistemo.dot
-dot -Tpng -o strukturo-kotizosistemo.png strukturo-kotizosistemo.dot
+dot -Tpng -o strukturo-malaligxkotizosistemo.png strukturo-malaligxkotizosistemo.dot  2>/dev/null
+dot -Tpng -o strukturo-kotizosistemo.png strukturo-kotizosistemo.dot 2>/dev/null
 dot -Tpng -o datumbazstrukturo.png datumbazstrukturo.dot
+
+dot -Tpng -o programstrukturo.png programstrukturo.dot
